f9773d8e032ca67748b502f195d075bcb2a25825
Samantha Seng <samanthaseng@Samanthas-MacBook-Pro.local>
2022-09-25 19:51:22 -0700

    Final Homework

--- surfsup/app.py
+++ surfsup/app.py
@@ -33,16 +33,18 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-###### Everything you need here can be found in Day 3 Activity 10
+
 @app.route("/")
 def welcome():
-    return (f"Available routes<br/>"
+    return (f"Welcome!<br/>"
+            f"-------------------------------<br/>"
+            f"Available Routes<br/></br>"
             f"/api/v1.0/precipitation<br/>"
             f"/api/v1.0/stations<br/>"
             f"/api/v1.0/tobs<br/>"
-            f"/api/v1.0/start<br/>"
-            f"/api/v1.0/startdate/enddate<br/><br/><br/>"
-            f"Start and end date should be formatted as 'yyyy-mm-dd'")
+            f"/api/v1.0/<start>enter any date between <strong>2010-01-01 and 2017-08-23</strong> in 'YYYY-MM-DD' format<br/>"
+            f"/api/v1.0/<startdate>enter <strong>start date </strong>'YYYY-MM-DD' format/enter <strong>end date</strong> \
+            'YYYY-MM-DD' format<br/><br/><br/>")
 
 ###### the 'precipitation' route you will query and return the data Day 3 Activity 10
 @app.route("/api/v1.0/precipitation")
@@ -62,35 +64,56 @@ def stations():
     session.close()
     return jsonify(station_data)
 
-###### the 'tobs' route you will query and return the data Day 3 Activity 10
+###### Query dates and temp observations of most active station for the previous year of data
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
     session = Session(engine)
-    temperature = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date>="2016-08-23").filter(Measurement.date<="2017-08-23").all()
+    temperature = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date>="2016-08-23").\
+        filter(Measurement.date<="2017-08-23").all()
+    
     temp_dict = list(np.ravel(temperature))
     session.close()
     return jsonify(temp_dict)
 
-
-###### the 'temp' route you will query the data with params in the url and return the data Day 3 Activity 10
-@app.route("/api/v1.0/temp/<start>")
-def start_date():
+# Query dates and temp of the most active station for previous year of data
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    list = []
     session = Session(engine)
-    temp_start=session.query()
+    temperature = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                  filter(Measurement.date >= start).group_by(Measurement.date).all()
 
-@app.route("/api/v1.0/temp/<start>/<end>")
-def stats(start=None, end=None):
-    """Return TMIN, TAVG, TMAX."""
+    session.close()
+    for data in temperature:
+        dict = {}
+        dict['Date'] = data[0]
+        dict['TMIN'] = data[1]
+        dict['TAVG'] = round(data[2],2)
+        dict['TMAX'] = data[3]
+        list.append(dict)
 
-    # Select statement
+    #Return a JSON list
+    return jsonify(list)
+    
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+    session = Session(engine)
+    list = []
+    session = Session(engine)
+    temperature = session.query(Measurement.date,func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                  filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).all()
 
+    session.close()
+    for data in temperature:
+        dict = {}
+        dict['Date'] = data[0]
+        dict['TMIN'] = data[1]
+        dict['TAVG'] = round(data[2],2)
+        dict['TMAX'] = data[3]
+        list.append(dict)
 
-    # calculate TMIN, TAVG, TMAX with start and stop
-
-
-    # Unravel results into a 1D array and convert to a list
-
-
+    #Return a JSON list
+    return jsonify(list)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
